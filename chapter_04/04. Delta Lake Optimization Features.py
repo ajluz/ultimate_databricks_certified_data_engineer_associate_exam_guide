90e621e6a243e5c1c8414875482ab5c1407a6565
ajluz <ajluz>
2025-09-26 20:34:07 +0000

    commit

--- chapter_04/04. Delta Lake Optimization Features.py
+++ chapter_04/04. Delta Lake Optimization Features.py
@@ -4,16 +4,6 @@
 
 # COMMAND ----------
 
-# MAGIC %sql USE workspace.default;
-
-# COMMAND ----------
-
-spark.sql("drop table if exists tb_people_2")
-spark.sql("drop table if exists tb_people_2_partitioned")
-spark.sql("drop table if exists tb_people_3")
-
-# COMMAND ----------
-
 df = spark.read.load('/databricks-datasets/learning-spark-v2/people/people-10m.delta')
 df.write.option('maxRecordsPerFile', 1000).saveAsTable('default.tb_people_2')
 
@@ -24,6 +14,11 @@ spark.sql("""
     FROM tb_people_2
     WHERE gender = 'F'
 """).show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Table Partitioning
 
 # COMMAND ----------
 
@@ -73,6 +68,11 @@ spark.sql("""
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ### Using OPTIMIZE
+
+# COMMAND ----------
+
 # Note: Changing target file size to aproximately 2MB only for demonstrations; in production, larger targets (e.g., 128–512 MB) usually deliver better scan performance and fewer files.
 spark.sql("""
     ALTER TABLE tb_people_2
@@ -109,7 +109,7 @@ spark.sql("""
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ### Handling High Cardinality Filters
+# MAGIC ### Handling High Cardinality Filters by using Z-Ordering
 
 # COMMAND ----------
 
@@ -133,7 +133,7 @@ spark.sql("""
 spark.sql("""
     SELECT id, firstName, gender, ssn, salary
     FROM tb_people_2
-    WHERE id IN (99999)
+    WHERE id = 99999
 """).show()
 
 # COMMAND ----------
@@ -158,11 +158,3 @@ spark.sql("""
     "clusteringColumns"
 ).show()
 
-
-# COMMAND ----------
-
-spark.sql("""
-    SELECT id, firstName, gender, ssn, salary
-    FROM tb_people_2
-    WHERE id IN (99999)
-""").show()
