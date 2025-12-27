a187a5173495caa01e932504fe0dc8aee499cbc5
ajluz <ajluz>
2025-12-27 16:37:00 +0000

    commit

--- chapter_03/demo_notebook.py
+++ chapter_03/demo_notebook.py
@@ -8,7 +8,24 @@ print("Hello World")
 
 # COMMAND ----------
 
-
+# MAGIC %md
+# MAGIC # Title 1
+# MAGIC ## Title 2
+# MAGIC
+# MAGIC This is regular text. **This is bold text.** *This is italic text.*
+# MAGIC
+# MAGIC Below is a list:
+# MAGIC - Item 1
+# MAGIC - Item 2
+# MAGIC - Item 3
+# MAGIC
+# MAGIC This is an embedded link: <a href="https://en.wikipedia.org/wiki/Markdown" target="_blank">What is Markdown?</a>
+# MAGIC
+# MAGIC Below is a table:
+# MAGIC | Name   | Age |
+# MAGIC |--------|-----|
+# MAGIC | Arthur | 34  |
+# MAGIC
 
 # COMMAND ----------
 
